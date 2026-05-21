2c34308f14a2514c6d208c94e225f035e7e15e42
RzWojtek <116317334+RzWojtek@users.noreply.github.com>
2026-05-21 14:58:32 +0200

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,31 @@
-const CACHE = 'xpost-v1'
+const CACHE = 'xpost-v2'
 const STATIC = ['/', '/index.html']
 
-self.addEventListener('install', e => e.waitUntil(
-  caches.open(CACHE).then(c => c.addAll(STATIC))
-))
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(STATIC)))
+  self.skipWaiting()
+})
 
-self.addEventListener('fetch', e => e.respondWith(
-  caches.match(e.request).then(r => r || fetch(e.request))
-))
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', e => {
+  // Zawsze próbuj pobrać świeżą wersję z sieci
+  // Cache tylko jako fallback gdy brak połączenia
+  e.respondWith(
+    fetch(e.request)
+      .then(response => {
+        // Zapisz świeżą wersję do cache
+        const clone = response.clone()
+        caches.open(CACHE).then(c => c.put(e.request, clone))
+        return response
+      })
+      .catch(() => caches.match(e.request))
+  )
+})
